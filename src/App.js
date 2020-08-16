220577aae04ac8dae855dc6478f97de210558607
Yung-Shin Lin <yungshin@msn.com>
2020-08-15 17:43:26 -0700

    Work around AWS

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there and here (in Git)!
+          Hi there and here from Feature Branch!
         </p>
         <a
           className="App-link"
